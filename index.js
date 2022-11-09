90ca0c034260cf203e7c4fb9cd3a56bb36dc25dd
kamruzzaman22874 <joybd.eee@gmail.com>
2022-11-09 17:09:00 +0600

    Added review section

--- index.js
+++ index.js
@@ -43,7 +43,16 @@ async function run() {
 								res.send(service);
 
 
-						})
+							})
+				app.get('/reviews', async (req, res) => {
+					let query = {};
+					if (req.query.email) {
+						query = { email: req.query.email };
+					}
+					const cursor = reviewCollection.find(query);
+					const reviews = await cursor.toArray();
+					res.send(reviews);
+				});
 
 						app.post('/services', async (req, res) => {
 							const services = req.body;
